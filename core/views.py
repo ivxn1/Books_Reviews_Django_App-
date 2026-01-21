37442a36c1f0b943e52f964bc37b7ef73b501acd
Ivan Zhelev <ivxn.zhelev@gmail.com>
2026-01-21 19:31:59 +0200

    Established landing page and its view, conntected index to project's urls

--- core/views.py
+++ core/views.py
@@ -1,6 +1,6 @@
+from django.db import models
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
-
 from books.models import Book
 from reviews.models import Review
 
@@ -8,7 +8,10 @@ from reviews.models import Review
 # Create your views here.
 
 def index(request: HttpRequest) -> HttpResponse:
-    latest_books = Book.objects.order_by('-published_date')[:3]
+    latest_books = Book.objects.annotate(
+        average_rating=models.Avg('reviews__rating')
+    ).order_by('-publishing_date')[:3]
+
     latest_reviews = Review.objects.order_by('-created_at')[:3]
 
     context = {
